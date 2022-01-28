98b4030c59ae4e4a6cf6a74081f6894374a1415b
Jeet <jitendrashivmandir@gmail.com>
2022-01-28 14:58:05 +0530

    cart pages add

--- server.js
+++ server.js
@@ -4,11 +4,7 @@ const ejs = require('ejs')
 const path = require('path')
 const expressLayout = require('express-ejs-layouts')
 const PORT = process.env.PORT || 3000  //? it is checking 3000 port is assign or not 
-app.get('/',(req, res)=>{
-    // const pp = (path.join(__dirname) + '/resources/Views')
-    // res.send(pp)
-    res.render('home');
-})
+
 //~# Assets
 app.use(express.static('Public'))
 
@@ -16,6 +12,24 @@ app.use(express.static('Public'))
 app.use(expressLayout);
 const jj = app.set('views',path.join(__dirname) + '/resources/Views')
 app.set('view engine', 'ejs');
+
+//~* routes setting
+//# when we are use layout make sure this routes always below the template engine
+app.get('/',(req, res)=>{
+    res.render('home');
+})
+app.get('/cart',(req, res)=>{
+    res.render('customers/cart.ejs');
+})
+
+app.get('/login',(req, res)=>{
+    res.render('auth/login.ejs');
+})
+
+app.get('/register',(req, res)=>{
+    res.render('auth/register.ejs');
+})
+
 app.listen(PORT, ()=>{
     console.log(`Listening on Port ${PORT}`)
 })
